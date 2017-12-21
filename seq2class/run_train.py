5aae1bff1546249774f20803a62a5fb5f17508fd
feizhihui <helloworld@csu.edu.cn>
2017-12-21 10:52:35 +0800

    add mixed train way

--- seq2class/run_train.py
+++ seq2class/run_train.py
@@ -5,7 +5,7 @@ from sklearn import metrics
 from sequence_model import SeqModel
 
 batch_size = 512
-epoch_num = 5
+epoch_num = 7
 show_step = 200
 keep_prob = 0.75
 # ===================================
